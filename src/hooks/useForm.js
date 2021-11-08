3bf2ce0a91a0e170fc82c0ad8187bd4144e0d3c9
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-07 23:02:16 -0500

    created and implemented custom hook to control the checkout form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+
+const useForm = (initialFormValues, initialBoolean) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(initialBoolean);
+    const [values, setValues] = useState(initialFormValues);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(!showSuccessMessage);
+    };
+
+    return [values, showSuccessMessage, handleChanges, handleSubmit];
+}
+
+export default useForm;
